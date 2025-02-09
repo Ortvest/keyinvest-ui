996e128ce59f581dc21481c3d39804fea85e6710
Maksymus333333 <mdachevskyi@edu.cdv.pl>
2025-02-09 16:09:51 +0100

    [KIAI-38] feat: installed the @uidotdev/usehooks library

--- src/modules/Auth/ThemeToggle/index.tsx
+++ src/modules/Auth/ThemeToggle/index.tsx
@@ -1,12 +1,13 @@
 import { useEffect } from 'react';
 
 import classNames from 'classnames';
-import useLocalStorage from 'use-local-storage';
 
 import DarkMode from '@shared/assets/icons/DarkMode.svg';
 import LightMode from '@shared/assets/icons/LightMode.svg';
 
 import './styles/styles.css';
+
+import { useLocalStorage } from '@uidotdev/usehooks';
 
 export const ThemeToggle = (): JSX.Element => {
   const [theme, setTheme] = useLocalStorage('theme', 'light');
