eb9ff88262f5d026eb89c9d5df993343e9afa023
GormanProg123 <gparfenchik2005@gmail.com>
2025-02-18 13:23:27 +0100

    [KIAI-37] ref: ENV correct

--- src/modules/Auth/shared/api/base.api.ts
+++ src/modules/Auth/shared/api/base.api.ts
@@ -1,6 +1,8 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
+const baseUrl = import.meta.env.VITE_API_URL;
+
 export const baseApi = createApi({
-  baseQuery: fetchBaseQuery({ baseUrl: 'https://keytrading-backend-production.up.railway.app/api' }),
+  baseQuery: fetchBaseQuery({ baseUrl }),
   endpoints: () => ({}),
 });
