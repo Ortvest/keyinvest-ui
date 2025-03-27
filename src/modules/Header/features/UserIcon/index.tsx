b66058cda3328d323b037ca77f0d30f49e1d3545
GormanProg123 <gparfenchik2005@gmail.com>
2025-03-27 03:38:33 +0100

    [KIAI-59] feat: user icon

---
+++ src/modules/Header/features/UserIcon/index.tsx
@@ -0,0 +1,33 @@
+import { useNavigate } from 'react-router-dom';
+
+import './styles/styles.css';
+
+interface UserIconProps {
+  email: string;
+}
+
+export const UserIcon = ({ email }: UserIconProps): JSX.Element => {
+  const navigate = useNavigate();
+  const firstLetter = email.charAt(0).toUpperCase();
+
+  const getStableColor = (input: string): string => {
+    let hash = 0;
+    for (let i = 0; i < input.length; i++) {
+      hash = input.charCodeAt(i) + ((hash << 5) - hash);
+    }
+    const color = `#${(hash & 0x00ffffff).toString(16).toUpperCase().padStart(6, '0')}`;
+    return color;
+  };
+
+  const backgroundColor = getStableColor(email);
+
+  const onHandleClick: React.MouseEventHandler<HTMLDivElement> = (): void => {
+    navigate('/system');
+  };
+
+  return (
+    <div className="user-icon" style={{ backgroundColor }} onClick={onHandleClick}>
+      {firstLetter}
+    </div>
+  );
+};
