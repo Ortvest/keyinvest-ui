985d5308075f128ace5968cbaa7175e46a9f2eff
Dmytro Kazantsev <d.kazantsev@codejet.ai>
2025-03-28 06:36:30 +0100

    [KIAI-40] feat: validation for login

---
+++ src/shared/validation/sign-in.schema.ts
@@ -0,0 +1,10 @@
+import * as yup from 'yup';
+
+export const signInSchema = yup.object().shape({
+  email: yup.string().email('Invalid email').required('Email is required'),
+  password: yup.string().required('Password is required'),
+});
+
+export const signInEmailOnlySchema = yup.object().shape({
+  email: yup.string().email('Invalid email').required('Email is required'),
+});
